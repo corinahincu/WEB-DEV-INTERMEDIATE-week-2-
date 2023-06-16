2f54bab3073135f669f1202bfcfc15bafdaa28bb
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-06-16 15:32:21 +0200

    Add files via upload

---
+++ bonus-google-drive-arrays/JS-SNOW/flake.js
@@ -0,0 +1,12 @@
+// OOP : Classes -> Object / Constructor
+//  cum sa faci din clase obiecte prin constructor. Constructorul este o metoda speciala care participa la initierea obiectului
+
+// class Person {
+// ...
+// constructor(name){
+//
+//}
+// ...
+//}
+
+// new Person("John")
